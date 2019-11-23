8a1c133401685925c01065fdb391242945d1e400
Apurv Tripathi <ap364626@dal.ca>
2019-11-23 16:52:17 -0400

    Removed toolbar

--- android/android_speech_recognition-master/app/src/main/java/com/zaidimarvels/voiceapp/MainActivity.java
+++ android/android_speech_recognition-master/app/src/main/java/com/zaidimarvels/voiceapp/MainActivity.java
@@ -39,8 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
