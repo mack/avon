0c6442deb3711aac6fa8422b919bb7d073548833
jarretterrio <jarretterrio@live.com>
2019-11-23 13:52:21 -0400

    Added reactive mic siri wave, needs storyboard

--- ios/avon/avon/Controller/HomeController.swift
+++ ios/avon/avon/Controller/HomeController.swift
@@ -11,7 +11,8 @@ import Speech
 import AVKit
 
 class HomeController: UIViewController {
-
+    
+    private var recorder: AVAudioRecorder!
     let audioEngine: AVAudioEngine? = AVAudioEngine()
     let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
     let request = SFSpeechAudioBufferRecognitionRequest()
@@ -37,7 +38,7 @@ class HomeController: UIViewController {
         siriWave = SiriWaveView(frame: CGRect(x: 0, y:(UIScreen.main.bounds.height/2) - 100 , width: UIScreen.main.bounds.width, height: 200))
         
         self.view.addSubview(siriWave)
-        testWithoutMic()
+        setupRecorder()
     }
     
     private func testWithoutMic() {
@@ -56,6 +57,85 @@ class HomeController: UIViewController {
         }
     }
     
+    private func checkMicPermission() -> Bool {
+        var permissionCheck: Bool = false
+        
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case AVAudioSession.RecordPermission.granted:
+            permissionCheck = true
+        case AVAudioSession.RecordPermission.denied:
+            permissionCheck = false
+        case AVAudioSession.RecordPermission.undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission({ (granted) in
+                if granted {
+                    permissionCheck = true
+                } else {
+                    permissionCheck = false
+                }
+            })
+        default:
+            break
+        }
+        
+        return permissionCheck
+    }
+    
+    /// Recorder Setup Begin
+       @objc func setupRecorder() {
+           if(checkMicPermission()) {
+               startRecording()
+           } else {
+               print("permission denied")
+           }
+       }
+    
+    @objc func updateMeters() {
+        var normalizedValue: Float
+        recorder.updateMeters()
+        normalizedValue = normalizedPowerLevelFromDecibels(decibels: recorder.averagePower(forChannel: 0))
+        self.siriWave.update(CGFloat(normalizedValue) * 30)
+    }
+    
+    private func normalizedPowerLevelFromDecibels(decibels: Float) -> Float {
+           let minDecibels: Float = -60.0
+           if (decibels < minDecibels || decibels.isZero) {
+               return .zero
+           }
+           
+           let powDecibels = pow(10.0, 0.05 * decibels)
+           let powMinDecibels = pow(10.0, 0.05 * minDecibels)
+           return pow((powDecibels - powMinDecibels) * (1.0 / (1.0 - powMinDecibels)), 1.0 / 2.0)
+           
+       }
+
+    private func startRecording() {
+        let recordingSession = AVAudioSession.sharedInstance()
+        let recorderSettings = [AVSampleRateKey: NSNumber(value: 44100.0),
+                                AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless),
+                                AVNumberOfChannelsKey: NSNumber(value: 2),
+                                AVEncoderAudioQualityKey: NSNumber(value: Int8(AVAudioQuality.min.rawValue))]
+        
+        let url: URL = URL(fileURLWithPath:"/dev/null")
+        do {
+            
+            let displayLink: CADisplayLink = CADisplayLink(target: self,
+                                                           selector: #selector(HomeController.updateMeters))
+            displayLink.add(to: RunLoop.current,
+                            forMode: RunLoop.Mode.common)
+
+            try recordingSession.setCategory(.playAndRecord,
+                                             mode: .default)
+            try recordingSession.setActive(true)
+            self.recorder = try AVAudioRecorder.init(url: url,
+                                                     settings: recorderSettings as [String : Any])
+            self.recorder.prepareToRecord()
+            self.recorder.isMeteringEnabled = true;
+            self.recorder.record()
+            print("recorder enabled")
+        } catch {
+            print("recorder init failed")
+        }
+    }
     
     func requestSpeechAuthorization() {
         SFSpeechRecognizer.requestAuthorization { authStatus in
