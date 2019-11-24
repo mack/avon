1bd6ecbe34edbea249ff2f393d34ed85a43a6a13
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2019-11-23 23:21:47 -0400

    Added swipable pages

--- avon/Controller/SettingsController.swift
+++ avon/Controller/SettingsController.swift
@@ -14,6 +14,7 @@ class SettingsController: PullUpController {
     var commands = ["Call", "Text", "Reminder", "Speed Warnings"]
     var tableView: UITableView?
     var hintLabel: UILabel?
+    var pageControl: UIPageControl?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,28 @@ class SettingsController: PullUpController {
         self.view.layer.cornerRadius = 10
         self.view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
         
-        let titleView = UILabel(frame: CGRect(x: 35, y: 20, width: self.view.bounds.width - 35, height: 100))
-        titleView.font = UIFont.boldSystemFont(ofSize: 32.0)
-        titleView.text = "Commands"
-        self.view.addSubview(titleView)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 50))
+        pageControl?.pageIndicatorTintColor = UIColor.init(red: 214, green: 214, blue: 214)
+        pageControl?.currentPageIndicatorTintColor = .black
+        pageControl?.numberOfPages = 2
+        pageControl!.currentPage = 0
+        self.view.addSubview(pageControl!)
+        
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 40, width: self.view.bounds.width, height: self.view.bounds.height - 40), collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellView")
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        collectionView.bounces = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.contentInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
+        self.view.addSubview(collectionView)
+
         
         hintLabel = UILabel(frame: CGRect(x: 0, y: -65, width: self.view.bounds.width, height: 100))
         hintLabel?.font = UIFont.systemFont(ofSize: 11.0)
@@ -33,17 +52,8 @@ class SettingsController: PullUpController {
         hintLabel?.alpha = 0.4
         hintLabel?.textColor = .white
         
-        self.view.addSubview(titleView)
         self.view.addSubview(hintLabel!)
-        
-        tableView = UITableView(frame: CGRect(x: 21, y: 105, width: self.view.bounds.width - 40, height: UIScreen.main.bounds.height - 105))
 
-        self.view.addSubview(tableView!)
-        tableView!.isScrollEnabled = false
-        tableView!.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView?.rowHeight = 50
-        tableView?.dataSource = self
-        tableView?.separatorColor = .clear
         portraitSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 100)
     }
     
@@ -93,4 +103,46 @@ extension SettingsController: UITableViewDataSource {
         return cell
       }
 
+}
+
+
+extension SettingsController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.parent!.view.frame.width, height: self.parent!.view.frame.height)
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+         let offset = scrollView.contentOffset.x
+         if (offset >= self.view.frame.width) {
+            self.pageControl!.currentPage = 1
+         } else {
+            self.pageControl!.currentPage = 0
+         }
+     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellView", for: indexPath)
+        if (indexPath.row == 0) {
+            let titleView = UILabel(frame: CGRect(x: 35, y: 0, width: cell.bounds.width - 35, height: 100))
+            titleView.font = UIFont.boldSystemFont(ofSize: 32.0)
+            titleView.text = "Commands"
+            
+            tableView = UITableView(frame: CGRect(x: 21, y: 75, width: self.view.bounds.width - 40, height: UIScreen.main.bounds.height - 75))
+            tableView!.isScrollEnabled = false
+            tableView!.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+            tableView?.rowHeight = 50
+            tableView?.dataSource = self
+            tableView?.separatorColor = .clear
+
+            cell.addSubview(tableView!)
+            cell.addSubview(titleView)
+        } else {
+            var view = UIView(frame: cell.bounds)
+            cell.addSubview(view)
+        }
+        return cell
+    }
 }
